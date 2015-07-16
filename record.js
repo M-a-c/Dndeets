19f6e81089b3b2ab637df29b322f1cd44f05fbe8
Kevin Nadro <sodapopcuffgames@gmail.com>
2015-07-15 19:22:58 -0500

    record.js
    - stores the character information
    - almost done with the first page, just need to add more information for
    the skills then its done

---
+++ record.js
@@ -0,0 +1,335 @@
+var sheet = {
+
+	/*
+		BASIC CHARACTER INFORMATION
+	*/
+
+	"character_name": "  ",
+	
+	"player": "  ",
+	
+	"class": "  ",
+
+	"level": "  ",
+
+	"career_level": "  ",
+
+	"race": "  ",
+
+	"alignment": "  ",
+
+	"deity": "  ",
+
+	"size": "  ",
+
+	"age": "  ",
+
+	"gender": "  ",
+
+	"height": "  ",
+
+	"weight": "  ",
+
+	"eyes": "  ",
+
+	"hair": "  ",
+
+	"skin": "  ",
+
+
+	/* 
+		Ability Names
+	*/
+
+	"ability_name":{
+
+		"str": {
+			"ability_score": "  ",
+			"ability_mod": "  ",
+			"temp_score": "  ",
+			"temp_mod": "  ",
+		},
+
+		"dex": {
+			"ability_score": "  ",
+			"ability_mod": "  ",
+			"temp_score": "  ",
+			"temp_mod": "  ",
+		},
+
+		"con": {
+			"ability_score": "  ",
+			"ability_mod": "  ",
+			"temp_score": "  ",
+			"temp_mod": "  ",
+		},
+
+		"int": {
+			"ability_score": "  ",
+			"ability_mod": "  ",
+			"temp_score": "  ",
+			"temp_mod": "  ",
+		},
+
+		"wis": {
+			"ability_score": "  ",
+			"ability_mod": "  ",
+			"temp_score": "  ",
+			"temp_mod": "  ",
+		},
+
+		"cha": {
+			"ability_score": "  ",
+			"ability_mod": "  ",
+			"temp_score": "  ",
+			"temp_mod": "  ",
+		},
+
+	},
+
+
+	/*
+		Hit Points
+	*/
+
+	"hit_points": {
+
+		"total": "  ",
+		"current_HP": "  ",
+		"non_lethal_damage": "  ",
+
+	},
+
+	/*
+		Armor Class
+	*/
+
+	"armor_class":{
+
+		"total": "  ",
+		"base" : "10",
+		"armor_bonus": "  ",
+		"shield_bonus": "  ",
+		"dex_mod": "  ",
+		"size_mod": "  ",
+		"natural_armor": "  ",
+		"defelection": "  ",
+		"misc_mod": "  ",
+
+	},
+
+
+	/* 
+		Damage reduction
+	*/
+
+	"damage_reduction": "  ",
+
+
+	/*
+		Speed
+	*/
+	"speed": "  ",
+
+	"touch": "  ",
+
+	"flat-footed": "  ",
+
+	/*
+		Initiative
+	*/
+	"initiative":{
+		"total": "  ",
+		"dex_mod": "  ",
+		"misc_mod": "  ",
+	},
+
+
+	/*
+		Saving Throws
+	*/
+	"saving_throws":{
+		"fortitude": {
+			"total": "  ",
+			"base_save": "  ",
+			"ability_mod": "  ",
+			"magic_mod": "  ",
+			"misc_mod": "  ",
+			"temp_mod": "  ", 
+		},
+		"reflex":{
+			"total" : "  ",
+			"base_save": "  ",
+			"ability_mod": "  ",
+			"magic_mod": "  ",
+			"misc_mod": "  ",
+			"temp_mod": "  ", 
+		},
+		"will": {
+			"total" : "  ",
+			"base_save": "  ",
+			"ability_mod": "  ",
+			"magic_mod": "  ",
+			"misc_mod": "  ",
+			"temp_mod": "  ", 
+		},
+	},
+
+
+	/*
+		Conditional modifiers not included for saving throws
+	*/
+
+
+	/*
+		Base attack bonus
+	*/
+
+	"base_attack_bonus": "  ",
+
+	/*
+		Spell resistance
+	*/
+
+	"spell_resistance": "  ",
+
+
+	/*
+		Grapple
+	*/
+
+	"grapple":{
+		"total": "  ",
+		"base_attack_bonus": "  ",
+		"strength_mod": "  ",
+		"size_mod": "  ",
+		"misc_mod": "  ",
+	},
+
+
+	/*
+		Attacks
+	*/	
+
+	"attack_1":{
+		"attack": "  ",
+		"attack_bonus": "  ",
+		"damage": "  ",
+		"critical": "  ",
+		"range": "  ",
+		"type": "  ",
+		"notes": "  ",
+		"ammunation": "  ",
+
+	},
+
+	"attack_2":{
+		"attack": "  ",
+		"attack_bonus": "  ",
+		"damage": "  ",
+		"critical": "  ",
+		"range": "  ",
+		"type": "  ",
+		"notes": "  ",
+		"ammunation": "  ",
+
+	},
+
+	"attack_3":{
+		"attack": "  ",
+		"attack_bonus": "  ",
+		"damage": "  ",
+		"critical": "  ",
+		"range": "  ",
+		"type": "  ",
+		"notes": "  ",
+		"ammunation": "  ",
+
+	},
+
+	"attack_4":{
+		"attack": "  ",
+		"attack_bonus": "  ",
+		"damage": "  ",
+		"critical": "  ",
+		"range": "  ",
+		"type": "  ",
+		"notes": "  ",
+		"ammunation": "  ",
+
+	},
+
+	"attack_5":{
+		"attack": "  ",
+		"attack_bonus": "  ",
+		"damage": "  ",
+		"critical": "  ",
+		"range": "  ",
+		"type": "  ",
+		"notes": "  ",
+		"ammunation": "  ",
+
+	},
+
+
+	/*
+		Skills
+	*/
+
+	"skills":{
+
+		"appraise":{},
+		"balance":{},
+		"bluff":{},
+		"climb":{},
+		"concentration":{},
+		"craft_1":{},
+		"craft_2":{},
+		"craft_3":{},
+		"decipher_script":{},
+		"diplomacy":{},
+		"disable_device":{},
+		"disguise":{},
+		"escape_artist":{},
+		"forgery":{},
+		"gather_information":{},
+		"handle_animal":{},
+		"heal":{},
+		"hide":{},
+		"intimidate":{},
+		"jump":{},
+		"knowledge_1":{},
+		"knowledge_2":{},
+		"knowledge_3":{},
+		"knowledge_4":{},
+		"knowledge_5":{},
+		"listen":{},
+		"move_silently":{},
+		"open_lock":{},
+		"perform_1":{},
+		"perform_2":{},
+		"perform_3":{},
+		"profession_1":{},
+		"profession_2":{},
+		"ride":{},
+		"search":{},
+		"sense_motive":{},
+		"sleight_of_hand":{},
+		"spellcraft":{},
+		"spot":{},
+		"survival":{},
+		"swim":{},
+		"tumble":{},
+		"use_magic_device":{},
+		"use_rope":{},
+		"blank_1":{},
+		"blank_2":{},
+		"blank_3":{},
+
+	},
+
+
+
+
+
+};
