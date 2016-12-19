d6215ceac564dfcc9acb968806269c575b5e293f
Kevin Nadro <sodapopcuffgames@gmail.com>
2016-12-19 14:02:49 -0600

    updating storage for firebase from dom elements
    - this will change just committing my work i have so far

--- storage.js
+++ storage.js
@@ -22,7 +22,10 @@ function initStorage() {
 	//populateLanguages();
 }
 
-
+/**
+  * Reads the current skills from the DOM and stores them in a temp object
+  * @param currentSheet - sheet object to store in the database
+  */
 function storageSkills(currentSheet){
 
 	var currentSkill;
@@ -41,37 +44,42 @@ function storageSkills(currentSheet){
 		 currentSheet.skills[String(skill)].skill_mod    = currentSkill.childNodes[3].childNodes[0].value;
 		 currentSheet.skills[String(skill)].ability_mod  = currentSkill.childNodes[6].childNodes[0].value;
 		 currentSheet.skills[String(skill)].ranks        = currentSkill.childNodes[9].childNodes[0].value;
-		 currentSheet.skills[String(skill)].misc_mod     = currentSkill.childNodes[12].childNodes[0].valu;
+		 currentSheet.skills[String(skill)].misc_mod     = currentSkill.childNodes[12].childNodes[0].value;
 
 	}
-    console.log(currentSheet.skills);
-}
-
-
-function populateBio(){
-
-	var currentSheet = sheet;
-
-	document.getElementsByName("characterName")[0].value  = currentSheet.character_name;
-	document.getElementsByName("playerName")[0].value     = currentSheet.player;
-	document.getElementsByName("classAndLevel")[0].value  = currentSheet.class + "," + String(currentSheet.level);
-	document.getElementsByName("race")[0].value 		  = currentSheet.race;
-	document.getElementsByName("alignment")[0].value 	  = currentSheet.alignment;
-	document.getElementsByName("deity")[0].value 		  = currentSheet.deity;
-	document.getElementsByName("SIZE")[0].value 		  = currentSheet.size;
-	document.getElementsByName("AGE")[0].value			  = currentSheet.age;
-	document.getElementsByName("GENDER")[0].value		  = currentSheet.gender;
-	document.getElementsByName("HEIGHT")[0].value		  = currentSheet.height;
-	document.getElementsByName("WEIGHT")[0].value		  = currentSheet.weight;
-	document.getElementsByName("EYES")[0].value		      = currentSheet.eyes;
-	document.getElementsByName("HAIR")[0].value 		  = currentSheet.hair;
-	document.getElementsByName("SKIN")[0].value 		  = currentSheet.skin;
 
 }
 
-function populateAbilites(){
+/**
+  * Reads the current bio from the DOM and stores them in a temp object
+  * @param currentSheet - sheet object to store in the database
 
-	var currentSheet = sheet;
+  */
+function storageBio(currentSheet){
+
+	currentSheet.character_name = document.getElementsByName("characterName")[0].value
+	currentSheet.player         = document.getElementsByName("playerName")[0].value
+	currentSheet.race           =  document.getElementsByName("race")[0].value
+	currentSheet.alignment      =  document.getElementsByName("alignment")[0].value
+	currentSheet.deity          =  document.getElementsByName("deity")[0].value
+	currentSheet.size           =  document.getElementsByName("SIZE")[0].value
+	currentSheet.age            =   document.getElementsByName("AGE")[0].value
+	currentSheet.gender         =  document.getElementsByName("GENDER")[0].value
+	currentSheet.height         =  document.getElementsByName("HEIGHT")[0].value
+	currentSheet.weight         =  document.getElementsByName("WEIGHT")[0].value
+	currentSheet.eyes           =  document.getElementsByName("EYES")[0].value
+	currentSheet.hair           =  document.getElementsByName("HAIR")[0].value
+	currentSheet.skin           =  document.getElementsByName("SKIN")[0].value
+    currentSheet.class + "," + String(currentSheet.level) = document.getElementsByName("classAndLevel")[0].value
+
+}
+
+/**
+  * Reads the current abilities from the DOM and stores them in a temp object
+  * @param currentSheet - sheet object to store in the database
+  */
+function storageAbilites(currentSheet){
+
 	var ability_score;
 	var ability_mod;
 	var temp_score;
@@ -79,23 +87,24 @@ function populateAbilites(){
 
 	for( ability in currentSheet.ability_name){
 
+		ability_score = document.getElementsByName(String(ability))[0].childNodes[3].childNodes[0].value;
+		ability_mod   = document.getElementsByName(String(ability))[0].childNodes[5].childNodes[0].value;
+		temp_score    = document.getElementsByName(String(ability))[0].childNodes[7].childNodes[0].value;
+		temp_mod      = document.getElementsByName(String(ability))[0].childNodes[9].childNodes[0].value;
 
-		ability_score = currentSheet.ability_name[String(ability)].ability_score;
-		ability_mod   = currentSheet.ability_name[String(ability)].ability_mod;
-		temp_score	  = currentSheet.ability_name[String(ability)].temp_score;
-		temp_mod	  = currentSheet.ability_name[String(ability)].temp_mod;
-
-		document.getElementsByName(String(ability))[0].childNodes[3].childNodes[0].value = ability_score;
-		document.getElementsByName(String(ability))[0].childNodes[5].childNodes[0].value = ability_mod;
-		document.getElementsByName(String(ability))[0].childNodes[7].childNodes[0].value = temp_score;
-		document.getElementsByName(String(ability))[0].childNodes[9].childNodes[0].value = temp_mod;
+        currentSheet.ability_name[String(ability)].ability_score = ability_sco;
+		currentSheet.ability_name[String(ability)].ability_mod   = ability_mod;
+		currentSheet.ability_name[String(ability)].temp_score    = temp_score;
+		currentSheet.ability_name[String(ability)].temp_mod      = temp_mod;
 
 	}
 }
 
-function populateSavingThrows(){
-
-	var currentSheet = sheet;
+/**
+  * Reads the current savingThrows from the DOM and stores them in a temp object
+  * @param currentSheet - sheet object to store in the database
+  */
+function storageSavingThrows(currentSheet){
 
 	var baseSave;
 	var ability_mod;
@@ -105,27 +114,28 @@ function populateSavingThrows(){
 
 	for(save in currentSheet.saving_throws){
 
-		baseSave 	= currentSheet.saving_throws[String(save)].base_save;
-		ability_mod = currentSheet.saving_throws[String(save)].ability_mod;
-		magic_mod   = currentSheet.saving_throws[String(save)].magic_mod;
-		misc_mod    = currentSheet.saving_throws[String(save)].misc_mod;
-		temp_mod    = currentSheet.saving_throws[String(save)].temp_mod;
+		baseSave    = document.getElementsByName(String(save))[0].childNodes[7].childNodes[1].value;
+		ability_mod = document.getElementsByName(String(save))[0].childNodes[11].childNodes[1].value;
+		magic_mod   = document.getElementsByName(String(save))[0].childNodes[15].childNodes[1].value;
+		misc_mod    = document.getElementsByName(String(save))[0].childNodes[19].childNodes[1].value;
+		temp_mod    = document.getElementsByName(String(save))[0].childNodes[23].childNodes[1].value;
 
-		document.getElementsByName(String(save))[0].childNodes[7].childNodes[1].value  = baseSave;
-		document.getElementsByName(String(save))[0].childNodes[11].childNodes[1].value = ability_mod;
-		document.getElementsByName(String(save))[0].childNodes[15].childNodes[1].value = magic_mod;
-		document.getElementsByName(String(save))[0].childNodes[19].childNodes[1].value = misc_mod;
-		document.getElementsByName(String(save))[0].childNodes[23].childNodes[1].value = temp_mod;
+        currentSheet.saving_throws[String(save)].base_save   = baseSave;
+        currentSheet.saving_throws[String(save)].ability_mod = ability_mod;
+        currentSheet.saving_throws[String(save)].magic_mod   = magic_mod;
+        currentSheet.saving_throws[String(save)].misc_mod    = misc_mod;
+        currentSheet.saving_throws[String(save)].temp_mod    = temp_mod;
 
 	}
 
 
 }
 
-
-function populateHealth(){
-
-	var currentSheet = sheet;
+/**
+  * Reads the current health from the DOM and stores them in a temp object
+  * @param currentSheet - sheet object to store in the database
+  */
+function storageHealth(currentSheet){
 
 	var total;
 	var current_HP;
@@ -133,22 +143,19 @@ function populateHealth(){
 	var wounds;
 	var speed;
 
-	total  = currentSheet.hit_points.total;
-	wounds = currentSheet.hit_points.wounds;
-	non_lethal_damage = currentSheet.hit_points.non_lethal_damage;
-	current_HP = currentSheet.hit_points.current_HP;
-	speed = currentSheet.speed;
+	total             = document.getElementById("Health").childNodes[1].childNodes[3].childNodes[3].value;
+	wounds            = document.getElementById("Health").childNodes[1].childNodes[5].childNodes[3].value;
+	current_HP        = document.getElementById("Health").childNodes[1].childNodes[7].childNodes[3].value;
+	non_lethal_damage = document.getElementById("Health").childNodes[1].childNodes[9].childNodes[3].value;
 
-	document.getElementById("Health").childNodes[1].childNodes[3].childNodes[3].value = total;
-	document.getElementById("Health").childNodes[1].childNodes[5].childNodes[3].value = wounds;
-	document.getElementById("Health").childNodes[1].childNodes[7].childNodes[3].value = current_HP;
-	document.getElementById("Health").childNodes[1].childNodes[9].childNodes[3].value = non_lethal_damage;
+    // Speed is here too
+	speed             = document.getElementById("Health").childNodes[1].childNodes[11].childNodes[3].value;
 
-	/*
-		Setting the speed
-	*/
-	document.getElementById("Health").childNodes[1].childNodes[11].childNodes[3].value = speed;
-
+    currentSheet.hit_points.total             = total;
+	currentSheet.hit_points.wounds            = wounds;
+	currentSheet.hit_points.non_lethal_damage = non_lethal_damage;
+	currentSheet.hit_points.current_HP        = current_HP;
+	currentSheet.speed                        = speed;
 
 }
 
